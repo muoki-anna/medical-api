cab6c6a73ef8c2a7d3838251c15dd4746f265879
DENNIS MUUO <dennismuuo.dev@gmail.com>
2026-05-14 09:11:46 +0300

    Audit: Implemented ClinicalAuditInterceptor for automatic, system-wide activity tracking at the API level

--- src/main/java/com/medicore/api/config/ClinicalAuditInterceptor.java
+++ src/main/java/com/medicore/api/config/ClinicalAuditInterceptor.java
@@ -8,8 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import java.util.Map;
-
 @Component
 public class ClinicalAuditInterceptor implements HandlerInterceptor {
 
